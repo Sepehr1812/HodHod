da5305e6fe58f62a7be3b1e1ef54cf19eaffb875
sepehr <sepehrakhoundi1812@gmail.com>
2021-11-06 15:56:31 +0330

    initial commit

---
+++ app/src/main/java/ir/hodhod/hodhod/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.hodhod.hodhod
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
